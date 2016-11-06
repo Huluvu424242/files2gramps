6ba8c5a5fe115141abd8a71f18231c791f4d5f00
FunThomas424242 <funthomas424242@gmail.com>
2016-11-06 18:31:28 +0100

    configuration support added
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

--- src/test/java/com/github/funthomas424242/jpacker4gramps/FileHelperTest.java
+++ src/test/java/com/github/funthomas424242/jpacker4gramps/FileHelperTest.java
@@ -6,18 +6,33 @@ import static org.junit.Assert.assertTrue;
 import java.io.File;
 import java.io.IOException;
 
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.builder.fluent.Configurations;
+import org.apache.commons.configuration2.ex.ConfigurationException;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class FileHelperTest {
 
     protected FileHelper fileHelper;
 
+    protected static Configuration config;
+
+    @BeforeClass
+    public static void init() {
+        Configurations configs = new Configurations();
+        try {
+            config = configs.properties(new File("test.properties"));
+        } catch (ConfigurationException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Before
     public void setUp() {
-        final File file = new File(
-                "src/test/resources/beispiel1/database/ahnen1.txt");
-        assertTrue(file.exists());
+        final File file = new File(config.getString("gramps.file"));
+        assertTrue("gramps.file does not exists:", file.exists());
         fileHelper = new FileHelper(file);
     }
 
