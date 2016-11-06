5daa3f68d0947652f400d51a9c89cdb9b71e899a
FunThomas424242 <funthomas424242@gmail.com>
2016-11-06 21:31:30 +0100

    prepare tests for exporter
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

--- src/main/java/com/github/funthomas424242/jpacker4gramps/GrampsExporter.java
+++ src/main/java/com/github/funthomas424242/jpacker4gramps/GrampsExporter.java
@@ -1,19 +1,49 @@
 package com.github.funthomas424242.jpacker4gramps;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class GrampsExporter {
 
     protected File grampsFile;
-    protected File mediaFolder;
     protected File targetArchive;
 
-    public GrampsExporter(final File grampsFile, final File mediaFolder,
-            final File targetArchive) {
+    protected File mediaFolder;
+
+    public GrampsExporter(final File grampsFile, final File targetArchive,
+            final File mediaFolder) {
 
         this.grampsFile = grampsFile;
         this.mediaFolder = mediaFolder;
         this.targetArchive = targetArchive;
     }
 
+    public GrampsExporter(final File grampsFile, final File targetArchive) {
+
+        this.grampsFile = grampsFile;
+        this.mediaFolder = null;
+        this.targetArchive = targetArchive;
+    }
+
+    protected void createArchivefile() {
+
+    }
+
+    protected void addGampsFile() {
+
+    }
+
+    protected void addMediaFolderFiles() {
+
+    }
+
+    protected void writeArchiveFileAndClose() {
+
+    }
+
+    public FileInputStream createExportfile() throws FileNotFoundException {
+
+        return new FileInputStream(this.targetArchive);
+    }
 }
